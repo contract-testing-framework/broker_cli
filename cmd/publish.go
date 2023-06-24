ab551ae59e9f9027ff41d19255d6d7d0d66c5001
Zach Morgan <zachmorgan97@gmail.com>
2023-06-24 13:47:38 -0700

    Publish command POST's a contract with proper payload format, and enforces all requirements. Still needs some refactoring.
    Co-authored-by: HD <HernandoD@users.noreply.github.com>

--- cmd/publish.go
+++ cmd/publish.go
@@ -4,8 +4,11 @@ import (
 	"errors"
 	"log"
 	"os"
+	"os/exec"
 	"net/http"
 	"bytes"
+	"fmt"
+	"encoding/json"
 
 	"github.com/spf13/cobra"
 )
@@ -15,6 +18,13 @@ var check = func(err error) {
 		log.Fatal(err)
 	}
 }
+
+var Type string;
+var Branch string;
+var ProviderName string;
+var Version string;
+var ContractFormat string;
+var Contract []byte;
 
 var publishCmd = &cobra.Command{
 	Use:   "publish",
@@ -44,25 +54,145 @@ flags:
 			return errors.New("Two arguments are required")
 		}
 
-		/*
-		publish <path to pact> <brokerURL> sends a .json pact to broker
-		*/
 		path := args[0]
-		brokerURL := args[1]
+
+		if Type != "consumer" && Type != "provider" {
+			if len(Type) == 0 {
+				Type = "not set"
+			}
+			msg := fmt.Sprintf("--type required to be \"consumer\" or \"provider\", --type was %v", Type)
+			return errors.New(msg)
+		}
+
+		var name string
+		if Type == "provider" && len(ProviderName) == 0 {
+			return errors.New("Must set --provider-name if --type is \"provider\"")
+		}
+
+		if Type == "provider" {
+			name = ProviderName
+		} else {
+			// type is consumer, get consumer name from contract
+
+			type consumer struct{
+				Name string `json:"name"`
+			}
+
+			type pact struct{
+				Consumer consumer `json:"consumer"`
+				Interactions interface{} `json:"interactions"`
+				MetaData interface{} `json:"metadata"`
+				Provider interface{} `json:"provider"`
+			}
+
+			contractBytes, err := os.ReadFile(path)
+			check(err)
+
+			var contract pact
+			err = json.Unmarshal(contractBytes, &contract)
+			check(err)
+
+			name = contract.Consumer.Name
+		}
+
+		if len(Version) == 0 {
+			return errors.New("Must set --version")
+		}
+
+		type Body struct{
+			ContractType string `json:"contractType"`
+			Contract interface{} `json:"contract"`
+			ParticipantName string `json:"participantName"`
+			ParticipantVersion string `json:"participantVersion"`
+			ParticipantBranch string `json:"participantBranch"`
+			ContractFormat string `json:"contractFormat"`
+		}
+
+		var contractBytes []byte
+		var format string
+		var contract interface{}
+
+		if path[(len(path) - 4):] == "json" {
+			format = "json"
+
+			var err error
+			contractBytes, err = os.ReadFile(path)
+			check(err)
+
+			err = json.Unmarshal(contractBytes, &contract)
+			check(err)
+
+		} else if path[(len(path) - 4):] == "yaml" || path[(len(path) - 3):] == "yml" {
+			format = "yaml"
+
+			var err error
+			contractBytes, err = os.ReadFile(path)
+			check(err)
+			contract = string(contractBytes)
+			check(err)
+		} else {
+			return errors.New("Contract must be either JSON or YAML")
+		}
 		
-		contract, err := os.ReadFile(path)
+		brokerURL := args[1]
+
+		requestBody := Body{
+			ContractType: Type,
+			Contract: contract,
+			ParticipantName: name,
+			ParticipantVersion: Version,
+			ParticipantBranch: Branch,
+			ContractFormat: format,
+		}
+
+		jsonData, err := json.Marshal(requestBody)
 		check(err)
-		bodyReader := bytes.NewReader(contract)
+
+		bodyReader := bytes.NewBuffer(jsonData) // io.Reader interface type
 
 		resp, err := http.Post(brokerURL, "application/json", bodyReader)
 		check(err)
 		defer resp.Body.Close();
 
+		if resp.StatusCode != 201 {
+			type respError struct{
+				Error string `json:error`
+			}
+	
+			var respBody respError
+			err = json.NewDecoder(resp.Body).Decode(&respBody)
+			check(err)
+
+			if respBody.Error == "Participant version already exists" {
+				respBody.Error = respBody.Error + "\n\nA new participant version must be set whenever a contract is published."
+			}
+
+			fmt.Printf("Status code: %v\n", resp.Status)
+			log.Fatal(respBody.Error)
+		}
 
 		return nil
 	},
 }
 
 func init() {
+	cmd := exec.Command("git", "rev-parse", "--short=10", "HEAD")
+	gitSHA, err := cmd.Output()
+	check(err)
+
+	// trim off trailing newline
+	gitSHA = gitSHA[:len(gitSHA) - 1]
+
 	rootCmd.AddCommand(publishCmd)
+	publishCmd.Flags().StringVarP(&Type, "type", "t", "", "Type of contract (\"consumer\" or \"provider\")")
+	publishCmd.Flags().StringVarP(&Branch, "branch", "b", "", "Version control branch (optional)")
+	publishCmd.Flags().StringVarP(&ProviderName, "provider-name", "n", "", "The name of the provider service (required if --type is \"provider\")")
+	publishCmd.Flags().StringVarP(&Version, "version", "v", string(gitSHA), "The version of the service (Defaults to git SHA)")
+
+/*
+-v —version (optional)
+	- if no version is passed in, use the git branch short SHA
+	- API will return 4xx if the consumer version already exists, in that case, log a helpful error msg. (ex. try committing your changes to generate a new git SHA)
+*/
+
 }
