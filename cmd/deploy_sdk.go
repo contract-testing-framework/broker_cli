73af1bd2f648d8219eebb3020849cf538300ce23
Zach Morgan <zachmorgan97@gmail.com>
2023-07-13 16:37:25 -0700

    added 'signet undeploy command'

--- cmd/deploy_sdk.go
+++ cmd/deploy_sdk.go
@@ -1,12 +1,10 @@
 package cmd
 
 import (
-	// "bufio"
 	"errors"
 	"fmt"
 	"context"
 	"io/ioutil"
-	"log"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -23,32 +21,23 @@ const stackName = "signetbroker"
 var deploy_sdk = &cobra.Command{
 	Use:   "deploy_sdk",
 	Short: "Deploy the Signet broker to you AWS account on ECS with Fargate",
-	Long:  `Deploy the Signet broker to you AWS account on ECS with Fargate
-	
-	flags:
-
-	-s -—silent                (bool) silence docker's status updates as it provisions AWS infrastructure
-	`,
+	Long:  `Deploy the Signet broker to you AWS account on ECS with Fargate`,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// silent = viper.GetBool("deploy.silent")
-
-		cfg, err := config.LoadDefaultConfig(context.TODO())
-    if err != nil {
-			return errors.New("unable to load SDK config: " + err.Error())
-    }
-		
-		// need to get template from node module...
-		templateFile, err := ioutil.ReadFile("cftemplate4.yaml")
+		template, err := getCloudFormationTemplate()
 		if err != nil {
-			return errors.New("unable to load CloudFormation template: " + err.Error())
+			return err
 		}
-		template := string(templateFile)
 		
 		csInput := &cloudformation.CreateStackInput{
 			StackName: aws.String(stackName),
 			TemplateBody: aws.String(template),
 			Capabilities: []types.Capability{"CAPABILITY_IAM"},
 		}
+
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+    if err != nil {
+			return errors.New("unable to load SDK config - have you configured your aws cli with `aws configure`? " + err.Error())
+    }
 		
 		cfClient := cloudformation.NewFromConfig(cfg)
 		_, err = cfClient.CreateStack(context.TODO(), csInput)
@@ -56,24 +45,47 @@ var deploy_sdk = &cobra.Command{
 			return errors.New("unable to create CloudFormation stack: " + err.Error())
 		}
 
-		fmt.Println(colorGreen + "Deploying" + colorReset + " - deploying Signet broker to your AWS cloud using ECS with Fargate, this may take a few minutes...")
+		fmt.Println(colorGreen + "Deploying" + colorReset + " - deploying Signet broker to your AWS cloud using ECS with Fargate, this will take a few minutes...")
 
-		waiter := cloudformation.NewStackCreateCompleteWaiter(cfClient)
-		waitDuration, err := time.ParseDuration("15m") // 15 minutes
-		if err != nil {
+		if err := waitForDeploymentDone(cfClient); err != nil {
 			return err
-		}
-
-		name := stackName
-		dsInput := &cloudformation.DescribeStacksInput{StackName: &name}
-		if err := waiter.Wait(context.TODO(), dsInput, waitDuration); err != nil { // changed this from WaitForOutput before committing - untested
-			return errors.New("error while waiting for CloudFormation stack to be created - it is likely that Signet CLI simply timed out. Check your AWS console for the status of the deployment: " + err.Error())
 		}
 
 		fmt.Println(colorGreen + "Deployed Successfully")
 
 		return nil
 	},
+}
+
+func getCloudFormationTemplate() (string, error) {
+	signetRoot, err := getNpmPkgRoot()
+	if err != nil {
+		return "", errors.New("unable to find signet-cli global npm package: " + err.Error())
+	}
+	templatePath := signetRoot + "/cftemplate.yaml"
+	
+	templateFile, err := ioutil.ReadFile(templatePath)
+	if err != nil {
+		return "", errors.New("unable to load CloudFormation template: " + err.Error())
+	}
+	
+	return string(templateFile), nil
+}
+
+func waitForDeploymentDone(cfClient *cloudformation.Client) error {
+	waiter := cloudformation.NewStackCreateCompleteWaiter(cfClient)
+	waitDuration, err := time.ParseDuration("15m")
+	if err != nil {
+		return err
+	}
+
+	name := stackName
+	dsInput := &cloudformation.DescribeStacksInput{StackName: &name}
+	if err := waiter.Wait(context.TODO(), dsInput, waitDuration); err != nil { // changed this from WaitForOutput before committing - untested
+		return errors.New("error while waiting for CloudFormation stack to be created - it is likely that Signet CLI simply timed out. Check your AWS console for the status of the deployment: " + err.Error())
+	}
+
+	return nil
 }
 	
 func init() {
---
+++ cmd/undeploy.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"context"
+	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
+)
+
+var undeployCmd = &cobra.Command{
+	Use:   "undeploy",
+	Short: "Tear down the Signet broker deployment on AWS ECS",
+	Long:  `Tear down the Signet broker deployment on AWS ECS`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		dsInput := &cloudformation.DeleteStackInput{StackName: aws.String(stackName)}
+
+		cfg, err := config.LoadDefaultConfig(context.TODO())
+    if err != nil {
+			return errors.New("unable to load SDK config - have you configured your aws cli with `aws configure`? " + err.Error())
+    }
+		
+		cfClient := cloudformation.NewFromConfig(cfg)
+		_, err = cfClient.DeleteStack(context.TODO(), dsInput)
+		if err != nil {
+			return errors.New("unable to delete CloudFormation stack: " + err.Error())
+		}
+
+		fmt.Println(colorGreen + "Undeploying" + colorReset + " - tearing down the Signet broker deployment on AWS, this will take a few minutes...")
+
+		if err := waitForUndeploymentDone(cfClient); err != nil {
+			return err
+		}
+
+		fmt.Println(colorGreen + "Undeployed Successfully")
+
+		return nil
+	},
+}
+
+func waitForUndeploymentDone(cfClient *cloudformation.Client) error {
+	waiter := cloudformation.NewStackDeleteCompleteWaiter(cfClient)
+	waitDuration, err := time.ParseDuration("15m")
+	if err != nil {
+		return err
+	}
+
+	name := stackName
+	dsInput := &cloudformation.DescribeStacksInput{StackName: &name}
+	if err := waiter.Wait(context.TODO(), dsInput, waitDuration); err != nil { // changed this from WaitForOutput before committing - untested
+		return errors.New("error while waiting for CloudFormation stack to be deleted - it is likely that Signet CLI simply timed out. Check your AWS console for the status of the teardown operations: " + err.Error())
+	}
+
+	return nil
+}
+	
+func init() {
+	RootCmd.AddCommand(undeployCmd)
+}
