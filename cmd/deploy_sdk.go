d1c1fb15e5ce9d984070f6fd6ee893dc26d13ca9
Zach Morgan <zachmorgan97@gmail.com>
2023-07-13 15:48:11 -0700

    new deploy command waits until deployment completes before exiting

--- cmd/deploy_sdk.go
+++ cmd/deploy_sdk.go
@@ -2,26 +2,22 @@ package cmd
 
 import (
 	// "bufio"
-	// "errors"
+	"errors"
 	"fmt"
-	// "os/exec"
-	// "regexp"
 	"context"
 	"io/ioutil"
 	"log"
+	"time"
 
 	"github.com/spf13/cobra"
-	// "github.com/spf13/viper"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 
-	// "github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation/types"
 )
 
-// var silent bool
 const stackName = "signetbroker"
 
 var deploy_sdk = &cobra.Command{
@@ -36,14 +32,15 @@ var deploy_sdk = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// silent = viper.GetBool("deploy.silent")
 
-		cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+		cfg, err := config.LoadDefaultConfig(context.TODO())
     if err != nil {
-        log.Fatalf("unable to load SDK config, %v", err)
+			return errors.New("unable to load SDK config: " + err.Error())
     }
 		
+		// need to get template from node module...
 		templateFile, err := ioutil.ReadFile("cftemplate4.yaml")
 		if err != nil {
-			log.Fatalf("unable to load template file, %v", err)
+			return errors.New("unable to load CloudFormation template: " + err.Error())
 		}
 		template := string(templateFile)
 		
@@ -54,14 +51,26 @@ var deploy_sdk = &cobra.Command{
 		}
 		
 		cfClient := cloudformation.NewFromConfig(cfg)
-		output, err := cfClient.CreateStack(context.TODO(), csInput)
+		_, err = cfClient.CreateStack(context.TODO(), csInput)
 		if err != nil {
-			log.Fatalf("failed to create stack, %v", err)
+			return errors.New("unable to create CloudFormation stack: " + err.Error())
 		}
 
-		fmt.Println("Output from cf.CreateStack()")
-		fmt.Println(output.StackId)
-		fmt.Println(output.ResultMetadata)
+		fmt.Println(colorGreen + "Deploying" + colorReset + " - deploying Signet broker to your AWS cloud using ECS with Fargate, this may take a few minutes...")
+
+		waiter := cloudformation.NewStackCreateCompleteWaiter(cfClient)
+		waitDuration, err := time.ParseDuration("15m") // 15 minutes
+		if err != nil {
+			return err
+		}
+
+		name := stackName
+		dsInput := &cloudformation.DescribeStacksInput{StackName: &name}
+		if err := waiter.Wait(context.TODO(), dsInput, waitDuration); err != nil { // changed this from WaitForOutput before committing - untested
+			return errors.New("error while waiting for CloudFormation stack to be created - it is likely that Signet CLI simply timed out. Check your AWS console for the status of the deployment: " + err.Error())
+		}
+
+		fmt.Println(colorGreen + "Deployed Successfully")
 
 		return nil
 	},
@@ -69,8 +78,4 @@ var deploy_sdk = &cobra.Command{
 	
 func init() {
 	RootCmd.AddCommand(deploy_sdk)
-
-	// deploy_sdk.Flags().BoolVarP(&silent, "silent", "s", false, "silence docker's status updates as it provisions AWS infrastructure")
-
-	// viper.BindPFlag("deploy.silent", deploy_sdk.Flags().Lookup("silent"))
 }
